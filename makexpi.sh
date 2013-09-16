85358f790b03ef5c0bafdf86adab6cb2e9520bd1
Micah Lee <micah@eff.org>
2013-09-16 15:59:06 -0700

    improved makexpi.sh

--- makexpi.sh
+++ makexpi.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+APP_NAME=httpse-ruleset-tests
+XPI_NAME=$APP_NAME.xpi
 
 [ -d pkg ] || mkdir pkg
 
 cd src
-zip -r ../pkg/httpse-ruleset-tests.xpi * 
+zip -q -r ../pkg/$XPI_NAME * 
 cd ..
+
+echo "Package built in pkg/$XPI_NAME"
