06407013968c0ae4d71bec72d0c003b9566a8862
Lisa Yao <lisacyao@gmail.com>
2013-09-16 20:29:51 -0400

    trying to make popup work booooo

--- src/chrome/content/httpseRulesetTests.js
+++ src/chrome/content/httpseRulesetTests.js
@@ -1,3 +1,15 @@
 function httpse_ruleset_tests_run() {
-    alert('running tests');
+
+    strbundle = document.getElementById("strings");
+    var nofilesfound=strbundle.getString("installAlert");
+
+    alert(nofilesfound);var HTTPSEverywhere = null;
+    try {
+        HTTPSEverywhere = Components.classes["@eff.org/https-everywhere;1"]
+            .getService(Components.interfaces.nsISupports)
+            .wrappedJSObject;
+    } catch(e) {
+        alert('Please install HTTPS Everywhere and run again');
+    }
+
 }
